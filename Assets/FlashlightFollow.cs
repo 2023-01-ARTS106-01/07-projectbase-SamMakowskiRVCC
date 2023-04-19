be58c574c1015171467e17a5204e0c26b92d476d
SamMakowskiRVCC <113396202+SamMakowskiRVCC@users.noreply.github.com>
2023-04-19 11:48:11 -0400

    flashlight

---
+++ Assets/FlashlightFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightFollow : MonoBehaviour
+{
+    [SerializeField] GameObject thingToFollow;
+    // this things position (flashlight) should be the same as the FPC's position
+
+    void LateUpdate()
+    {
+        transform.position = thingToFollow.transform.position 
+                                 + new Vector3 (0,0.8f,0);
+        
+    }
+
+}
